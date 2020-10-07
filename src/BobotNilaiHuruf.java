dba509fccd7df71deeda4115159cb27d0165765c
AlbaniKhamenei <72059584+AlbaniKhamenei@users.noreply.github.com>
2020-10-07 19:57:35 +0700

    Update BobotNilaiHuruf.java

--- src/BobotNilaiHuruf.java
+++ src/BobotNilaiHuruf.java
@@ -8,30 +8,30 @@
  *
  * @author Windows 10
  */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+public class BobotNilaiSwitch {
+     public static void main(String[] args){
+ Scanner input = new Scanner (System.in);
 
+ char huruf = input.next().charAt(0);
+ System.out.print("Nilai Huruf = ");
+ switch (huruf){
+ case 'A':
+ System.out.println("Nilai angka 4");
+ break;
+ case 'B':
+ System.out.println("Nilai angka 3");
+ break;
+ case 'C':
+ System.out.println("Nilai angka 2");
+ break;
+ case 'D':
+ System.out.println("Nilai angka 1");
+ break;
+ case 'E':
+ System.out.println("Nilai angka 0");
+ break;
 
-
-public class BobotNilaiHuruf {
-    public static void main(String [] args) throws IOException {
-        BufferedReader x=new BufferedReader (new InputStreamReader(System.in));
-        
-        String angka = "";
-        System.out.print("Nilai Huruf = ");
-        angka = x.readLine();
-        int na=0;
-        if (angka.equals("A"))
-            na=4;
-        else if (angka.equals("B"))
-             na=3;
-        else if (angka.equals("C"))
-             na=2;
-        else if (angka.equals("D"))
-             na=1;
-        else if (angka.equals("E"))
-             na=0;
-       System.out.println("Nilai Angka = " + na);
-    }
+ }
+ }
 }
